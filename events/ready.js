03cb829dd1b42d9065a0449fe3a9b75a105d1ae4
Janisky <25519922+Janisky@users.noreply.github.com>
2023-01-08 13:56:10 -0300

    Funcion para multi servidor

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,13 @@ module.exports = async (client) => {
 
     (async () => {
         try {
-            await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), { body: client.commands });
+
+            // Para uso en un solo servidor (guild) replazar el siguiente codigo por:
+            // await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), { body: client.commands });
+            // y Eliminar la siguiente linea
+
+            await rest.put(Routes.applicationCommands(client.user.id), { body: client.commands });
+            
             console.log(`registrando comandos de barra`)
         } catch (err) {
             console.log(err)
